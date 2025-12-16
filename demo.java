3c147c7259b9e92f52aad9dc7cfbc6463f65e01e
HARIBABU3598 <harirom5444v@gmail.com>
2025-12-16 11:46:17 +0530

    Added extra Enjoy coding print statement

--- demo.java
+++ demo.java
@@ -2,5 +2,6 @@ public class demo{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("This is a demo Java program.");
+        System.out.println("Enjoy coding!");
     }
 }
